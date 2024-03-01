57cd62a1f895f59f7fd74c0b3882b882485f1992
nwomn <rongziqian5@gmail.com>
2024-03-01 01:00:20 +0000

    Update to "ComPort" version2

---
+++ Form1.cs
@@ -0,0 +1,104 @@
+using System.IO.Ports;
+
+namespace ComPort
+{
+    public partial class Form1 : Form
+    {
+        SerialPort serialPort1 = new SerialPort();
+        string dataOUT;
+        string dataIN;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            cBoxCOMPORT.Items.AddRange(ports);
+            serialPort1.DataReceived += serialPort1_DataReceived;
+            chBoxAlwaysUpdate.Checked = false;
+            chBoxAddToOldData.Checked = true;
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = cBoxCOMPORT.Text;
+                serialPort1.BaudRate = Convert.ToInt32(cBoxBaudRate.Text);
+                serialPort1.DataBits = Convert.ToInt32(cBoxDataBits.Text);
+                serialPort1.StopBits = (StopBits)Enum.Parse(typeof(StopBits), cBoxStopBits.Text);
+                serialPort1.Parity = (Parity)Enum.Parse(typeof(Parity), cBoxParityBits.Text);
+
+                serialPort1.Open();
+                progressBar1.Value = 100;
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+                progressBar1.Value = 0;
+            }
+        }
+
+        private void btnSendData_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                dataOUT = tBoxDataOut.Text;
+                // serialPort1.WriteLine(dataOUT);
+                serialPort1.Write(dataOUT);
+            }
+        }
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            dataIN = serialPort1.ReadExisting();
+            this.Invoke(new EventHandler(ShowData));
+        }
+
+        private void ShowData(object? sender, EventArgs e)
+        {
+            if (chBoxAlwaysUpdate.Checked) {
+                tBoxDataIN.Text = dataIN;
+            }
+            else if (chBoxAddToOldData.Checked)
+            {
+                tBoxDataIN.Text += dataIN;
+            }
+        }
+
+        private void chBoxAlwaysUpdate_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chBoxAlwaysUpdate.Checked)
+            {
+                chBoxAlwaysUpdate.Checked = true;
+                chBoxAddToOldData.Checked = false;
+            } 
+            else
+            {
+                chBoxAddToOldData.Checked = true;
+            }
+        }
+
+        private void chBoxAddToOldData_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chBoxAddToOldData.Checked)
+            {
+                chBoxAlwaysUpdate.Checked = false;
+                chBoxAddToOldData.Checked = true;
+            }
+            else
+            {
+                chBoxAlwaysUpdate.Checked = true;
+            }
+        }
+    }
+}
